20f220b0729408026319043eaec8db647ce2651b
yglee <yglee@ksmartpia.com>
2022-11-12 08:02:34 +0900

    add Product

---
+++ src/main/java/com/waterleak/waterleak/model/product/Product.java
@@ -0,0 +1,25 @@
+package com.waterleak.waterleak.model.product;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.waterleak.waterleak.model.product
+ * @since : 2022/11/12
+ */
+@Entity
+@Table(schema = "products")
+public class Product {
+
+  public Product() {}
+
+  @Id
+  private Long id;
+
+  private String name;
+
+  private Double price;
+
+}
