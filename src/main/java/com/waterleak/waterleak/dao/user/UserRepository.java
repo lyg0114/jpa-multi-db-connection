e6ce62f4ae278b6185ecac7ed7747f52940897c0
yglee <yglee@ksmartpia.com>
2022-11-12 08:04:45 +0900

    add UserRepository

---
+++ src/main/java/com/waterleak/waterleak/dao/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.waterleak.waterleak.dao.user;
+
+import com.waterleak.waterleak.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.waterleak.waterleak.dao
+ * @since : 2022/11/12
+ */
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
