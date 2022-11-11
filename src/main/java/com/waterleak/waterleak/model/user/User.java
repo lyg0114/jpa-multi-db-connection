ca8ecb8af82dcf17f799c2daffe183db31799fcd
yglee <yglee@ksmartpia.com>
2022-11-12 08:00:15 +0900

    add User

---
+++ src/main/java/com/waterleak/waterleak/model/user/User.java
@@ -0,0 +1,32 @@
+package com.waterleak.waterleak.model.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.waterleak.waterleak.model.user
+ * @since : 2022/11/12
+ */
+@Entity
+@Table(schema = "users")
+public class User {
+
+  public User() {}
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  private int age;
+
+}
