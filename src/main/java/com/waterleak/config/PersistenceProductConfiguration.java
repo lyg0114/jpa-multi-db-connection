9bd9d6ce7ea01cd095dee3690b597f8e3c1d5e90
lyg0114 <whdnseowkd@naver.com>
2022-11-12 17:29:38 +0900

    @Profile 추가

--- src/main/java/com/waterleak/config/PersistenceProductConfiguration.java
+++ src/main/java/com/waterleak/config/PersistenceProductConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -21,13 +22,13 @@ import org.springframework.transaction.PlatformTransactionManager;
  * @since : 2022/11/12
  */
 @Configuration
-//@PropertySource({"classpath:application-${spring.profiles.active}.yml"})
 @PropertySource({"classpath:application.yml"})
 @EnableJpaRepositories(
     basePackages = "com.waterleak.dao.product",
     entityManagerFactoryRef = "productEntityManager",
     transactionManagerRef = "productTransactionManager"
 )
+@Profile("dev")
 public class PersistenceProductConfiguration {
   @Autowired
   private Environment env;
--- src/main/java/com/waterleak/config/PersistenceUserConfiguration.java
+++ src/main/java/com/waterleak/config/PersistenceUserConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -22,13 +23,13 @@ import org.springframework.transaction.PlatformTransactionManager;
  * @since : 2022/11/12
  */
 @Configuration
-//@PropertySource({"classpath:application-${spring.profiles.active}.yml"})
 @PropertySource({"classpath:application.yml"})
 @EnableJpaRepositories(
     basePackages = "com.waterleak.dao.user",
     entityManagerFactoryRef = "userEntityManager",
     transactionManagerRef = "userTransactionManager"
 )
+@Profile("dev")
 public class PersistenceUserConfiguration {
   @Autowired
   private Environment env;
