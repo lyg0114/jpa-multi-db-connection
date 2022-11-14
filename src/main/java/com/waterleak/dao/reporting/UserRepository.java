2b339cf23594d7630ae78b8e80c169f1bf78128d
lyg0114 <whdnseowkd@naver.com>
2022-11-14 22:50:33 +0900

    불필요 코드 제거

--- src/main/java/com/waterleak/dao/reporting/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.waterleak.dao.reporting;
-
-import com.waterleak.model.reporting.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
